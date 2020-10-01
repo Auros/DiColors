7df893858aec33995bb557c49fa1ad9d16aeab8a
Auros Nexus <aurosnex@gmail.com>
2020-10-01 00:36:39 -0400

    Initial Project Setup

---
+++ DiColors/Plugin.cs
@@ -0,0 +1,29 @@
+using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace DiColors
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static IPALogger Log { get; private set; }
+
+        [Init]
+        public Plugin(IPALogger logger)
+        {
+            Log = logger;
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
