d0e6de8d36d60d2af39a8567ada68ad7375fbb4b
Auros Nexus <aurosnex@gmail.com>
2020-10-01 02:59:01 -0400

    Basic Image Swapper

---
+++ DiColors/HarmonyPatches/ObjectPoolDecorator.cs
@@ -0,0 +1,24 @@
+using Zenject;
+using SiraUtil;
+using HarmonyLib;
+using UnityEngine;
+using IPA.Utilities;
+
+namespace DiColors.HarmonyPatches
+{
+	[HarmonyPatch(typeof(GameplayCoreBeatmapObjectPoolsInstaller), "InstallBindings")]
+	internal class ObjectPoolDecorator
+	{
+		internal static void Prefix(ref GameplayCoreBeatmapObjectPoolsInstaller __instance, ref NoteController ____normalBasicNotePrefab)
+		{
+			var cnv = ____normalBasicNotePrefab.GetComponent<ColorNoteVisuals>();
+			Plugin.Log.NullCheck(cnv);
+
+
+			var spriteRenderer = cnv.GetField<SpriteRenderer, ColorNoteVisuals>("_arrowGlowSpriteRenderer");
+			var tex = BeatSaberMarkupLanguage.Utilities.FindTextureInAssembly("DiColors.Resources.ArrowGlow2x.png");
+			var sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f), 460, 0, SpriteMeshType.Tight);
+			spriteRenderer.sprite = sprite;
+		}
+	}
+}
--- DiColors/Plugin.cs
+++ DiColors/Plugin.cs
@@ -1,4 +1,6 @@
 using IPA;
+using HarmonyLib;
+using System.Reflection;
 using IPALogger = IPA.Logging.Logger;
 
 namespace DiColors
@@ -7,23 +9,25 @@ namespace DiColors
     public class Plugin
     {
         internal static IPALogger Log { get; private set; }
+		private readonly Harmony _harmony;
 
         [Init]
         public Plugin(IPALogger logger)
         {
             Log = logger;
+			_harmony = new Harmony("dev.auros.dicolors");
         }
 
         [OnEnable]
         public void OnEnable()
         {
-            
+			_harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
         [OnDisable]
         public void OnDisable()
         {
-
+			_harmony.UnpatchAll();
         }
     }
 }
